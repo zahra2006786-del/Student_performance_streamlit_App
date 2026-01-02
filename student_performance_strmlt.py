a133f7af7400c817eeb1d6c555a0e07315bebec0
zahra2006786-del <zahra2006786@gmail.com>
2026-01-02 09:01:49 -0800

    Add files via upload

---
+++ student_performance_strmlt.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+# from sklearn.pipeline import Pipeline
+
+
+model = joblib.load('linear_model.joblib')
+scaler = joblib.load('scaler.joblib')
+
+st.markdown('# ***Know your performance***')
+df = pd.read_csv("Student_Performance.csv")
+hour = st.selectbox('how many Hours you studied ?',df['Hours Studied'].unique())
+prev_score = st.number_input('What is your previous score ?')
+
+extra_act = st.selectbox('Are you involved in Extracurricular Activities ?', df['Extracurricular Activities'].unique())
+no = 0 if extra_act == False else 1
+
+sleep = st.selectbox('How many hours do you sleep ?', df['Sleep Hours'].unique())
+sample = st.selectbox('Number of Sample question papers you practiced ?', df['Sample Question Papers Practiced'].unique())
+
+pred = pd.DataFrame({'Hours Studied': [hour], 'Previous Scores': [prev_score], 'Extracurricular Activities': [no], 'Sleep Hours': [sleep], 'Sample Question Papers Practiced':[sample]})
+scaled = scaler.transform(pred)
+
+if st.button('Predict'):
+    raw_index = model.predict(scaled)[0]
+    
+    st.success("Your performance index: " + str(int(round(raw_index, 0))))
